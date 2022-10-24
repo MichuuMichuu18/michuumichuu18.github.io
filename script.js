798d30c8f377a81bb1bf8a5fc8b683404b99ea6f
MichuuMichuu18 <111843705+MichuuMichuu18@users.noreply.github.com>
2022-10-24 19:41:58 +0200

    added feedback after pushing monero button
    ^

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function copyMoneroAddress() {
   var value = "41hiNFCGava2NTK3uDQaNmFK7H7zgYJBdDXC9VSUBJ255XceKWhgVkT2w7xTc6caA9GH1fZobVv1k9n4EH8Gd5JtNWpznVr";
   navigator.clipboard.writeText(value);
+  alert("Copied!");
 } 
