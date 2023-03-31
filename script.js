a64494866832a1ccc2c7d52427ffc8f3f476b409
MichuuMichuu18 <111843705+MichuuMichuu18@users.noreply.github.com>
2023-03-31 12:43:32 +0200

    some small changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function copyMoneroAddress() {
   var value = "44D2frEXBV9TDUBss7PTYX9avDEfig2cEXidusq3okCLZfoHZ3eF3jVDqSDYWLPuWh3U1joRLBfXG3oF1hrFuZgkBM7zUN5";
   navigator.clipboard.writeText(value);
-  alert("Copied!");
+  alert("Copied wallet address!");
 } 
